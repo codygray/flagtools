1f2066a11f80e8040d1a07240897469bd21b7784
Cody Gray <cody@codygray.com>
2022-04-27 21:40:34 -0700

    Bug fix.

--- MonicasFlagToC.user.js
+++ MonicasFlagToC.user.js
@@ -4,7 +4,7 @@
 // @description   Implement https://meta.stackexchange.com/questions/305984/suggestions-for-improving-the-moderator-flag-overlay-view/305987#305987
 // @author        Cody Gray
 // @author        Shog9
-// @version       1.6.1
+// @version       1.6.2
 // @homepageURL   https://github.com/codygray/flagtools
 // @updateURL     https://github.com/codygray/flagtools/raw/codygray-updates/MonicasFlagToC.user.js
 // @downloadURL   https://github.com/codygray/flagtools/raw/codygray-updates/MonicasFlagToC.user.js
@@ -250,7 +250,7 @@
          max-height: 40vh;
     ` : `
 
-   ` }
+    ` }
       }
 
       .mod-tools ul.flags > li
